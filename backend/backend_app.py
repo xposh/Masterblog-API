6bbf5f7d1d4cde9d51b609b019178db26451e030
Posh <posh@Mac.fritz.box>
2026-01-30 16:20:55 +0100

    Add Delete Endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -16,28 +16,47 @@ def get_posts():
 
 @app.route('/api/posts', methods=['POST'])
 def add_post():
-    # 1. Die Daten vom Client (Frontend/Postman) holen
+    # 1. get data from frontend
     data = request.get_json()
 
     # 2. Validierung: Fehlt Titel oder Inhalt?
     if 'title' not in data or 'content' not in data:
         return jsonify({"error": "Missing title or content"}), 400
 
-    # 3. Neue ID generieren (Länge der Liste + 1)
+    # 3. Neue ID =  (List length + 1)
     new_id = len(POSTS) + 1
 
-    # 4. Neues Dictionary erstellen
+    # 4. Create new dict.
     new_post = {
         "id": new_id,
         "title": data['title'],
         "content": data['content']
     }
 
-    # 5. In die Liste speichern
+    # 5. In Liste speichern
     POSTS.append(new_post)
 
-    # 6. Erfolg zurückmelden (mit Status 201)
+    # 6.  Status 201
     return jsonify(new_post), 201
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete_post(id):
+    # 1. Post in der Liste suchen
+    post_to_delete = None
+    for post in POSTS:
+        if post['id'] == id:
+            post_to_delete = post
+            break
+
+    # 2. Error Handling: In case the Post doesnt exist
+    if post_to_delete is None:
+        return jsonify({"error": f"Post with id {id} not found"}), 404
+
+    # 3. Den Post aus der Liste entfernen
+    POSTS.remove(post_to_delete)
+
+
+    return jsonify({"message": f"Post with id {id} has been deleted successfully."}), 200
 
 
 
